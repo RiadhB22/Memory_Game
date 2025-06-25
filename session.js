d107096ec1ced88837186110540e64cc3430b010
RiadhB22 <riadh.bejjar@gmail.com>
2025-06-26 00:52:47 +0100

    Update session.js

--- session.js
+++ session.js
@@ -1,4 +1,3 @@
-// session.js
 import { getDatabase, ref, get, update } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
 export async function detectPlayerRole() {
@@ -12,7 +11,6 @@ export async function detectPlayerRole() {
   if (data.sessions?.joueur2 === sessionId) return "joueur2";
 
   const nom = prompt(data.sessions?.joueur1 ? "Entrez votre nom (Joueur 2) :" : "Entrez votre nom (Joueur 1) :");
-
   if (!nom) return null;
 
   if (!data.sessions?.joueur1) {
